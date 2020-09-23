1b4bb0ed4e66b1933a4a075a15d3c379c7834965
hyunduk-jo <johd321@kakao.com>
2020-09-23 21:12:40 +0900

    update join.pug

--- Controller/globalController.js
+++ Controller/globalController.js
@@ -5,7 +5,24 @@ export const home = (req, res) => res.render("home", { pageTitle: "Home" });
 export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
 export const postLogin = (req, res) => res.redirect(routes.home)
 
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+}
+export const postJoin = (req, res) => {
+  const {
+    body: { email, password1, password2, name, phoneNumber }
+  } = req;
+  if (password1 !== password2) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    res.redirect(routes.home);
+  }
+
+}
+
 export const welcome = (req, res) => res.render("welcome", { pageTitle: "Welcome" });
+
 export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+
 export const shoppingBasket = (req, res) => res.render("shoppingBasket", { pageTitle: "Basket" });
--- Router/globalRouter.js
+++ Router/globalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { home, join, postLogin, getLogin, search, welcome, shoppingBasket } from "../Controller/globalController";
+import { home, postLogin, getLogin, search, welcome, shoppingBasket, postJoin, getJoin } from "../Controller/globalController";
 import routes from "../routes";
 
 const globalRouter = express.Router();
@@ -9,7 +9,9 @@ globalRouter.get(routes.home, home);
 globalRouter.get(routes.login, getLogin);
 globalRouter.post(routes.login, postLogin)
 
-globalRouter.get(routes.join, join);
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(`${routes.join}${routes.welcome}`, welcome);
 globalRouter.get(routes.search, search);
 globalRouter.get(routes.shoppingBasket, shoppingBasket);
